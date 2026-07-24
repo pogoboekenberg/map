6de9bd3e870485f64224a2be9292a68ceeeeab54
fieldresearch <fieldresearchpogo@gmail.com>
2026-07-25 00:03:47 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,14 @@
-const CACHE_NAME = "pogo-boekenberg-20260724-63";
-const APP_SHELL = [
-  "./index.html",
-  "./styles.css",
-  "./config.js",
-  "./app.js",
-  "./manifest.webmanifest",
-  "./version.json",
-  "./assets/app-icon-180.png",
-  "./assets/app-icon-192.png",
-  "./assets/app-icon-512.png"
-];
-
 self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+  event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith("pogo-boekenberg-")).map(key => caches.delete(key)))).then(() => self.clients.claim()));
 });
 
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
   const url = new URL(event.request.url);
   if (url.origin !== self.location.origin) return;
-  event.respondWith((async () => {
-    const cacheKey = event.request.mode === "navigate" ? "./index.html" : url.pathname;
-    try {
-      const response = await fetch(event.request);
-      if (response.ok) await caches.open(CACHE_NAME).then(cache => cache.put(cacheKey, response.clone()));
-      return response;
-    } catch {
-      return caches.match(cacheKey);
-    }
-  })());
+  event.respondWith(fetch(event.request));
 });
